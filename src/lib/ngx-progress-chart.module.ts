d584b9c1cf9267a931c5913da4910d1b1f3a971f
Sondos Marashdeh <sondos.marashdeh@Sondos-Marashdeh.local>
2024-10-22 16:12:26 +0300

    first commit

---
+++ src/lib/ngx-progress-chart.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { NgxProgressChartComponent } from './ngx-progress-chart.component';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    NgxProgressChartComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NgxProgressChartComponent
+  ]
+})
+export class NgxProgressChartModule { }
---
+++ src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-progress-chart
+ */
+
+export * from './lib/ngx-progress-chart.component';
+export * from './lib/ngx-progress-chart.module';
